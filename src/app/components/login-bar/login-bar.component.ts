38353a46ae6a6dc4895b729d68337b6700bef7f1
Yubin <yubinchen18@gmail.com>
2018-06-06 09:51:51 +0200

    Link to login and change a style

--- src/app/components/login-bar/login-bar.component.ts
+++ src/app/components/login-bar/login-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-login-bar',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-bar.component.scss']
 })
 export class LoginBarComponent implements OnInit {
+
+  loginUrl = environment.loginUrl;
 
   constructor() { }
 
